ed6c6298821f0dc71d9d1a86cf04f73ee0a3ea90
Dekoder-py <dekod3r.py@gmail.com>
2025-10-10 21:21:47 +1300

    add scroll indicator
    https://youtu.be/0YhcQpbjz2U

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ function App() {
     <>
       <div id='welcome' className='h-screen flex flex-col items-center justify-center text-center'>
         <h1 className='text-3xl font-bold'>Hi! I'm Kyle.</h1>
-        <h2 className='text-2xl'>Welcome to my corner of the internet!</h2>
+        <h2 className='pb-20 text-2xl'>Welcome to my corner of the internet!</h2>
+        <div id='mouse-scroll'>
+        </div>
       </div>
-
+      
       <div id='about' className='flex flex-col items-center justify-center text-center pb-20'>
         <h2 className='text-2xl font-bold'>About Me</h2>
         <p>I'm a 15 year old programmer based in New Zealand.</p>
