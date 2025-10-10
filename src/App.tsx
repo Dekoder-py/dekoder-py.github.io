4d10dae15c30dcfcc592cec4ee04afb8c02f6a1d
Dekoder-py <dekod3r.py@gmail.com>
2025-10-10 21:10:23 +1300

    add an about section and do some styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import './App.css'
 function App() {
   return (
     <>
-      <h1 className='text-3xl font-bold'>Hi! I'm Kyle.</h1>
-      <h2 className='text-2xl'>Welcome to my corner of the internet!</h2>
+      <div id='welcome' className='h-screen flex flex-col items-center justify-center text-center'>
+        <h1 className='text-3xl font-bold'>Hi! I'm Kyle.</h1>
+        <h2 className='text-2xl'>Welcome to my corner of the internet!</h2>
+      </div>
+
+      <div id='about' className='flex flex-col items-center justify-center text-center pb-20'>
+        <h2 className='text-2xl font-bold'>About Me</h2>
+        <p>I'm a 15 year old programmer based in New Zealand.</p>
+      </div>
+
     </>
   )
 }
