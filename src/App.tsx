ca4bb41edb33c5c88ab59bbe236e7084beebb93c
Dekoder-py <dekod3r.py@gmail.com>
2025-10-11 07:32:42 +1300

    reformat and add more to about me

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <div id='welcome' className='h-screen flex flex-col items-center justify-center text-center'>
-        <h1 className='text-3xl font-bold'>Hi! I'm Kyle.</h1>
-        <h2 className='pb-20 text-2xl'>Welcome to my corner of the internet!</h2>
-        <div id='mouse-scroll'>
-        </div>
-      </div>
-      
-      <div id='about' className='flex flex-col items-center justify-center text-center pb-20'>
-        <h2 className='text-2xl font-bold'>About Me</h2>
-        <p>I'm a 15 year old programmer based in New Zealand.</p>
+      <div
+        id="welcome"
+        className="h-screen flex flex-col items-center justify-center text-center"
+      >
+        <h1 className="text-3xl font-bold">Hi! I'm Kyle.</h1>
+        <h2 className="pb-20 text-2xl">
+          Welcome to my corner of the internet!
+        </h2>
+        <div id="mouse-scroll"></div>
       </div>
 
+      <div
+        id="about"
+        className="flex flex-col items-center justify-center text-center pb-20"
+      >
+        <h2 className="text-2xl font-bold">About Me</h2>
+        <p>
+          I'm a 15 year old programmer based in New Zealand. <br />I enjoy
+          programming, homelabbing, Raspberry Pis, and being a part of{" "}
+          <a href="https://hackclub.com" target="_blank">
+            Hack Club
+          </a>
+          !
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
