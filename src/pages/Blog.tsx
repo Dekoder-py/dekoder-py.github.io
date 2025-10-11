e84ef2e6abad52200315b57c6cdf793387aa4b3c
Dekoder-py <dekod3r.py@gmail.com>
2025-10-12 10:26:40 +1300

    add blog padding top

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,6 +1,6 @@
 export default function Blog() {
   return (
-    <div id="blog" className="h-screen flex flex-col space-y-8">
+    <div id="blog" className="h-screen flex flex-col space-y-8 pt-10">
       <h2 className="text-2xl font-bold">Blog</h2>
       <h3 className="text-xl font-bold">Coming Soon...</h3>
     </div>
