6c207a38802149e92e99d4fb15b3c47003b4e557
Dekoder-py <dekod3r.py@gmail.com>
2025-10-12 10:18:36 +1300

    split app into pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div
+      id="welcome"
+      className="h-screen flex flex-col items-center justify-center text-center space-y-8"
+    >
+      <img
+        className="mx-auto"
+        src="https://emoji.slack-edge.com/T0266FRGM/wave-club-penguin/0dcab120e2e8d96e.gif"
+        alt="waving orpheus"
+      />
+      <h1 className="text-3xl font-bold">Hi! I'm Kyle.</h1>
+      <h2 className="text-2xl">Welcome to my corner of the internet!</h2>
+    </div>
+  );
+}
