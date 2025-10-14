d8edef225092f5a1679292a3a55dfd48529eddd7
Dekoder-py <dekod3r.py@gmail.com>
2025-10-14 16:43:03 +1300

    add more to my setup

--- src/pages/MySetup.tsx
+++ src/pages/MySetup.tsx
@@ -6,10 +6,12 @@ export default function MySetup() {
     >
       <h1 className="text-3xl font-bold">My Setup / Gear</h1>
       <ul className="list-inside list-disc">
-        <li>M1 MacBook Air (2020)</li>
+        <li>M1 MacBook Air (2020) with lots of stickers from Hack Club</li>
         <li>Keychron V1 (With knob) or my MacBook keyboard</li>
         <li>Logi MX Master 3S or my MacBook trackpad</li>
         <li>Some DELL monitor (not sure what model)</li>
+        <li>iPhone 13 Mini</li>
+        <li>Apple Watch SE</li>
       </ul>
     </div>
   );
