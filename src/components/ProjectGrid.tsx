5a84e4889d902eac4247599a6a16cd7f496f7d8e
Dekoder-py <dekod3r.py@gmail.com>
2025-10-14 14:27:20 +1300

    update grid styling

--- src/components/ProjectGrid.tsx
+++ src/components/ProjectGrid.tsx
@@ -7,7 +7,7 @@ interface ProjectsGridProps {
 
 export function ProjectsGrid({ projects }: ProjectsGridProps) {
   return (
-    <div className="grid grid-cols-1">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
       {projects.map(project => (
         <ProjectCard key={project.name} project={project} />
       ))}
