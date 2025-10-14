29a9797bd3cece6766e7d53a96f1345dc43ba4d2
Dekoder-py <dekod3r.py@gmail.com>
2025-10-14 14:27:35 +1300

    update card styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,13 +8,13 @@ export function ProjectCard({ project }: ProjectCardProps) {
   return (
     <div
       id={project.name + "-card"}
-      className="border-4 p-8 border-solid border-latte-lavender dark:border-macchiato-lavender w-60 dark:bg-macchiato-surface-0 bg-latte-surface-0"
+      className="border-4 p-8 border-solid rounded-md border-latte-lavender dark:border-macchiato-lavender w-60 dark:bg-macchiato-surface-0 bg-latte-surface-0"
     >
       <a href={project.url} target="_blank">
         <img
           src={project.imgUrl}
           alt={"Thumbnail image for " + project.name}
-          className="border-2 border-solid border-latte-blue dark:border-macchiato-blue"
+          className="border-2 border-solid border-latte-blue rounded-sm dark:border-macchiato-blue"
         />
         <h1 className="font-bold pt-10">{project.name}</h1>
         <p>{project.description}</p>
