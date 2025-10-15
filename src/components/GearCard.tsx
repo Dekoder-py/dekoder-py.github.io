2c0233ca52f46e49e8206ddae8bbfd38259640ae
Dekoder-py <dekod3r.py@gmail.com>
2025-10-15 17:47:56 +1300

    update styling of cards and add links

--- src/components/GearCard.tsx
+++ src/components/GearCard.tsx
@@ -6,9 +6,20 @@ interface GearCardProps {
 
 export default function GearCard({ GearItem }: GearCardProps) {
   return (
-    <div id={GearItem.itemName + "-card"} className="">
-      <h1>{GearItem.itemName}</h1>
-      <p>{GearItem.desc}</p>
+    <div
+      id={GearItem.itemName + "-card"}
+      className="border-solid border-2 border-latte-lavender dark:border-macchiato-lavender
+    w-full max-w-sm
+    rounded-lg shadow-md p-4 flex flex-col
+    bg-latte-surface-0 dark:bg-macchiato-surface-0
+    hover:shadow-2xl hover:scale-[1.02]
+    transition-all duration-300 ease-in-out
+      "
+    >
+      <a href={GearItem.url} target="_blank">
+        <h1 className="text-lg font-semibold">{GearItem.itemName}</h1>
+        <p className="text-sm mt-1">{GearItem.desc}</p>
+      </a>
     </div>
   );
 }
