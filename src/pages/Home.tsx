911deeb7b7de0e08d92a1efe074db7799e058a29
Dekoder-py <dekod3r.py@gmail.com>
2025-11-14 12:58:57 +1300

    fix placeholder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,16 +16,25 @@ export default function Home() {
       <section id="about">
         <p className="px-8">
           I'm a 16 year old programmer from New Zealand. I make projects, like{" "}
-          <a>this project link needed</a>! I am fluent in python and C++ and
-          decent at Java. I'm a member of{" "}
-          <a href="hackclub.com" target="_blank" className="underline">
+          <a
+            href="https://pypi.org/project/restless-dungeon/"
+            className="underline"
+          >
+            this
+          </a>
+          ! I am fluent in python and C++ and decent at Java. I'm a member of{" "}
+          <a href="https://hack.club" target="_blank" className="underline">
             Hack Club
           </a>
           ! <br />I prefer working in the CLI to any GUI app (which is why I'm
           not the best at UI things XD <br /> I wrote my own neovim config,
           ghostty config, and tmux config, which are the main tools I use to
           program. You can find them on my{" "}
-          <a href="github.com/dekoder-py" target="_blank" className="underline">
+          <a
+            href="https://github.com/dekoder-py"
+            target="_blank"
+            className="underline"
+          >
             GitHub
           </a>
           !
