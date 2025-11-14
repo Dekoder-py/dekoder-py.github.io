33f56e9f2b111804c080cd38e88245be873f2ea8
Dekoder-py <dekod3r.py@gmail.com>
2025-11-14 15:28:19 +1300

    reword

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -27,8 +27,9 @@ export default function Home() {
             Hack Club
           </a>
           ! <br />I prefer working in the CLI to any GUI app (which is why I'm
-          not the best at UI things XD) <br /> I wrote my own neovim config,
-          ghostty config, and tmux config, which are the main tools I use to
+          not the best at UI things XD) <br /> 
+          I've also written my own neovim,
+          ghostty, and tmux configs, which are the main tools I use to
           program. You can find them on my{" "}
           <a
             href="https://github.com/dekoder-py"
