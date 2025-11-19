5dea818a881fa7579eabd7475250c3438d45e500
Dekoder-py <dekod3r.py@gmail.com>
2025-11-19 16:02:58 +1300

    add lastfm api thing

---
+++ api/lastfm.js
@@ -0,0 +1,17 @@
+export default async function handler(res) {
+  const USERNAME = "your-nemesis";
+  const API_KEY = process.env.LASTFM_API_KEY;
+
+  try {
+    const response = await fetch(
+      `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USERNAME}&api_key=${API_KEY}&format=json`
+    );
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error("Failed to fetch from LastFM:", err);
+    res.status(500).json({ error: "Failed to fetch Last.fm data" });
+  }
+}
+
