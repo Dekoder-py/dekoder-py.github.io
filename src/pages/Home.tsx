09452b59f3f9d6ec9996cd86e3c7b1e729e31e8d
Dekoder-py <dekod3r.py@gmail.com>
2025-11-21 11:58:27 +1300

    colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -73,10 +73,10 @@ export default function Home() {
           width={150}
           className="rounded"
         />
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold text-latte-text dark:text-macchiato-text">
           Hello! Welcome to codingcorner.dev!
         </h1>
-        <h2 className="text-2xl">You've reached my corner of the internet!</h2>
+        <h2 className="text-2xl text-latte-lavender dark:text-macchiato-lavender">You've reached my corner of the internet!</h2>
       </div>
       <section id="about">
         <p className="px-8">
@@ -114,7 +114,7 @@ export default function Home() {
           id="webring"
           className="my-8 items-center text-center flex flex-col"
         >
-          <p className="text-latte-peach dark:text-macchiato-peach">Check out other sites in the catppuccin webring!</p>
+          <p className="text-latte-rosewater dark:text-macchiato-rosewater">Check out other sites in the catppuccin webring!</p>
           <div id="webring stuuf" className="flex mt-2">
             <a href="https://ctp-webr.ing/codingcorner/previous">
               <svg
@@ -174,7 +174,7 @@ export default function Home() {
         <p id="quote">Click me for fun words ;)</p>
       </section>
 
-      <h2 className="text-2xl">The 88x31 Grid</h2>
+      <h2 className="text-2xl text-latte-lavender dark:text-macchiato-lavender">The 88x31 Grid</h2>
       <span>
         Copy my button code to add it to your site: <CopySnippet />
       </span>
